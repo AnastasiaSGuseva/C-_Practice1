3d7166fccc31f812ed071c782ea6cb140224ecad
Anastasia <Atlanika373@yandex.ru>
2022-07-10 04:34:05 +0500

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая принимает на вход
+//три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    if(a>c)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    if(b>c)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.WriteLine(c);
+    }
+}
